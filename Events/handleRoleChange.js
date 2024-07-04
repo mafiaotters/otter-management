66e51bac62fdf71e6e55b131393c433426a9e78c
sernoz <48032984+sernoz@users.noreply.github.com>
2024-07-04 16:19:41 +0200

    auto update role in firebase link to Discord

--- Events/handleRoleChange.js
+++ Events/handleRoleChange.js
@@ -1,17 +1,16 @@
 const db = require('../Loader/loadDatabase'); // Assurez-vous que cette importation est correcte et que la fonction est appelée pour obtenir l'instance de la base de données
 
-const timestamp = new Date().toISOString();
-
 const rolePermissions = {
-  "Copains des loutres": 1,
-  "Loutre Retraitée": 2,
-  "Loutre Naissante": 3,
-  "Loutre Mafieuse": 4,
+  "Le Parrain": 6,
   "Sottocapo": 5,
-  "Le Parrain": 6
+  "Loutre Mafieuse": 4,
+  "Loutre Naissante": 3,
+  "Loutre Retraitée": 2,
+  "Copains des loutres": 1
 };
 
 module.exports = async (bot, oldMember, newMember) => {
+  const timestamp = new Date().toISOString();
   const guildIds = ["675543520425148416", "653689680906420235"];
   const guildId = newMember.guild.id;
 
@@ -29,7 +28,7 @@ module.exports = async (bot, oldMember, newMember) => {
 
 
 
-  // Prendre la liste des rôles du type, et prendre le rôle le plus élevé
+  // Prendre la liste des rôles du gars, et prendre le rôle le plus élevé
 
   lostRoles.forEach(async lostRole => {
     if (rolePermissions[lostRole.name]) {
@@ -39,44 +38,33 @@ module.exports = async (bot, oldMember, newMember) => {
         .first();
 
       if (highestRemainingRole) {
-        console.log(`[${timestamp}] Rôle retiré à ${newMember.displayName}: haut rôle restant: ${highestRemainingRole.name}`);
+        console.log(`[${timestamp}] Rôle retiré à ${newMember.displayName}: ${lostRole.name}; haut rôle restant: ${highestRemainingRole.name}`);
+        // Update statut membre dans Firestore (Oui cette update servira parfois à rien. Vérificaiton utilité update plus tard)
+        await userDocRef.update({
+          currentRole: highestRemainingRole.name
+        });
       }else{
-        console.log(`[${timestamp}] Rôle retiré à ${newMember.displayName}: aucun rôle restant`);
+        console.log(`[${timestamp}] Rôle retiré à ${newMember.displayName}: ${lostRole.name}; aucun rôle important restant`);
       }
     }
   });
 
+gainedRoles.forEach(async gainedRole => {
+  if (rolePermissions[gainedRole.name]) {
+    const highestGainedRole = newRoles
+      .filter(role => rolePermissions[role.name])
+      .sort((a, b) => rolePermissions[b.name] - rolePermissions[a.name])
+      .first();
 
-
-  gainedRoles.forEach(async gainedRole => {
-    console.log('ROLE GAGNE: ' + gainedRole)
-    if (rolePermissions[gainedRole.name]) {
-      // Récupérer la priorité la plus élevée parmi les rôles existants
-      const highestExistingRolePriority = newMember.roles.cache
-        .filter(role => rolePermissions[role.name])
-        .reduce((max, role) => Math.max(max, rolePermissions[role.name]), 0);
-      console.log('ROLE PRIORIETE EXISTANTE: ' + highestExistingRolePriority)
-      console.log('ROLE PRIORIETE NOUVEAU: ' + rolePermissions[gainedRole.name])
-      // Vérifier si le nouveau rôle a une priorité supérieure
-      if (rolePermissions[gainedRole.name] > highestExistingRolePriority) {
-        console.log('ROLE PRIORIETE SUPERIEURE')
-        try {
-          const doc = await userDocRef.get();
-          if (doc.exists && doc.data().verified) {
-            // Actions pour un utilisateur vérifié avec un nouveau rôle de priorité supérieure
-            console.log(`[${timestamp}] Rôle ajouté à ${newMember.displayName}(lodestone vérifié): priorité supérieure à un rôle existant`)
-          } else {
-            // Actions pour un utilisateur non vérifié ou sans profil avec un nouveau rôle de priorité supérieure
-            console.log(`[${timestamp}] Rôle ajouté à ${newMember.displayName}(lodestone non vérifié): priorité supérieure à un rôle existant`)
-          }
-        } catch (error) {
-          console.error(`[${timestamp}] Erreur lors de la vérification du profil de l'utilisateur ${newMember.displayName}`, error);
-        }
-      } else {
-        // Le nouveau rôle n'a pas une priorité supérieure, vous pouvez choisir de ne rien faire ou de gérer ce cas spécifiquement
-        console.log(`[${timestamp}] Rôle ajouté à ${newMember.displayName}: priorité inférieure ou égale à un rôle existant`)
-      }
+    if (highestGainedRole) {
+      console.log(`[${timestamp}] Rôle ajouté à ${newMember.displayName}: ${gainedRole.name}; haut rôle actuel: ${highestGainedRole.name}`);
+      await userDocRef.update({
+        currentRole: highestGainedRole.name
+      });
+    } else {
+      console.log(`[${timestamp}] Rôle ajouté à ${newMember.displayName}: ${gainedRole.name}; aucun rôle significatif gagné`);
     }
-    newMember.roles.cache.clear();
-    oldMember.roles.cache.clear();
-  });}
+  }
+});
+
+;}
