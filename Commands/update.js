907f4477c272b484de2ae610271c296fac778e6d
sernoz <48032984+sernoz@users.noreply.github.com>
2024-07-05 12:50:49 +0200

    update command added

---
+++ Commands/update.js
@@ -0,0 +1,24 @@
+const db = require('../Loader/loadDatabase'); 
+
+module.exports = {
+    name: "update",
+    description: "Mise à jour du site web des loutres.",
+    permission: "Aucune",
+    dm: true,
+    category: "User",
+
+    async run(bot, interaction, args) {
+        // Liste des ID des utilisateurs autorisés
+        const allowedUsers = ['207992750988197889', '173439968381894656']; // Jungso, Sefa
+        // Vérifie si l'utilisateur est un administrateur ou s'il est dans la liste des utilisateurs autorisés
+        const isAdmin = interaction.member.permissions.has('ADMINISTRATOR');// Les admins
+        const isAllowedUser = allowedUsers.includes(interaction.user.id);
+    
+        // Vérifie l'autorisation
+        if (!isAdmin || !isAllowedUser) {
+            // Si l'utilisateur n'est ni admin ni dans la liste, on refuse l'exécution de la commande
+            return interaction.reply({ content: "Vous n'avez pas la permission d'utiliser cette commande.", ephemeral: true });
+        }
+        return interaction.reply({ content: "Mise à jour du site ", ephemeral: true })
+    }
+}
