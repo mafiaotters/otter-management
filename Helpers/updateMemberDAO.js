ef7b8c83a58716298af871f2e21f901e53c8f1f1
ARK-BLKCN <48032984+sernoz@users.noreply.github.com>
2024-09-03 18:06:10 +0200

    fix permission

--- Helpers/updateMemberDAO.js
+++ Helpers/updateMemberDAO.js
@@ -110,8 +110,8 @@ class FriendsMemberDAO
             new Member("assets/img/speakers/ochi.jpg", "Ochi", "Mochi", "Cinis Chimaeras", true, "Avatar2"),
             new Member("assets/img/speakers/flora.jpg", "Flora", "Mantis", "Cinis Chimaeras", false, "Avatar2"),
             new Member("assets/img/speakers/kajiya.jpg", "Kajiya", "Nahel", "Cinis Chimaeras", false, "NoAvatar2"),
-		      	new Member("assets/img/speakers/alys.jpg", "Alys", "Huin", "Cinis Chimaeras", false, "NoAvatar2"),
-			      new Member("", "Kayak", "Hamo", "Cinis Chimaeras", false, "NoAvatar2"),
+		    new Member("assets/img/speakers/alys.jpg", "Alys", "Huin", "Cinis Chimaeras", false, "NoAvatar2"),
+			new Member("", "Kayak", "Hamo", "Cinis Chimaeras", false, "NoAvatar2"),
             new Member("assets/img/speakers/ella.jpg", "Ella", "Danloce", "Cinis Chimaeras", false, "NoAvatar2"),
         );
     }
