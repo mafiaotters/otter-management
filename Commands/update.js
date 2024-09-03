b0fba6808968f6097b31de3138c71ed544764e58
ARK-BLKCN <48032984+sernoz@users.noreply.github.com>
2024-09-03 20:15:46 +0200

    fix

--- Commands/update.js
+++ Commands/update.js
@@ -1,6 +1,8 @@
 const db = require('../Loader/loadDatabase'); 
 const updateFunction = require('../Helpers/updateFunction');
 
+const lastUsed = {};
+
 module.exports = {
     name: "update",
     description: "Mise à jour du site web des loutres.",
@@ -8,8 +10,22 @@ module.exports = {
     dm: true,
     category: "User",
 
+  
+
     async run(bot, interaction, args) {
-        const timestamp = new Date().toISOString();
+        const userId = interaction.user.id;
+        const timestamp = new Date().getTime();
+        const cooldownPeriod = 60000; // Délai en millisecondes, ici 60 secondes
+    
+        // Vérifie si l'utilisateur a déjà utilisé la commande récemment
+        if (lastUsed[userId] && timestamp - lastUsed[userId] < cooldownPeriod) {
+            const timeLeft = ((cooldownPeriod - (timestamp - lastUsed[userId])) / 1000).toFixed(0);
+            return interaction.reply({ content: `Veuillez attendre ${timeLeft} secondes avant de réutiliser cette commande.`, ephemeral: true });
+        }
+    
+        // Met à jour le timestamp de la dernière utilisation pour cet utilisateur
+        lastUsed[userId] = timestamp;
+
         // Liste des ID des utilisateurs autorisés
         const allowedUsers = ['207992750988197889', '173439968381894656', '239407042182381588']; // Jungso, Sefa, Kaaz, compte test sefa
         const isAllowedUser = allowedUsers.includes(interaction.user.id);
@@ -20,7 +36,7 @@ module.exports = {
             return interaction.reply({ content: "Vous n'avez pas la permission d'utiliser cette commande.", ephemeral: true });
         }
 
-        console.log(`[${timestamp}]: Mise à jour du site web...`);
+        console.log(` Mise à jour du site web...`);
 
         interaction.deferReply({ ephemeral: true });
 
