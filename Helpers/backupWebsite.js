30d2a3f39079a847f7b1481b49d618ceb10087f0
ARK-BLKCN <48032984+sernoz@users.noreply.github.com>
2024-09-09 13:54:41 +0200

    backups reworked (incremental + timestamped)

--- Helpers/backupWebsite.js
+++ Helpers/backupWebsite.js
@@ -1,23 +1,23 @@
 const fsExtra = require('fs-extra');
 const fs = require('fs').promises;
 const path = require('path');
-const SftpClient = require('ssh2-sftp-client'); // Utilisez ssh2-sftp-client pour SFTP
+const SftpClient = require('ssh2-sftp-client');
 
 async function backupWebsite() {
-    const timestamp = new Date().toISOString();
-    const backupFolderPath = path.join('./backupWebsite');
+    const timestamp = new Date(new Date().getTime() + (2 * 60 * 60 * 1000)).toISOString().replace(/:/g, '-');
+    const backupFolderName = `backup-${timestamp}`; // Nom du dossier de sauvegarde basé sur la date et l'heure
+    const backupFolderPath = path.join('./backups', backupFolderName);
 
-    console.log(`[${timestamp}]: Création d'une sauvegarde du site web du FTP...`)
+    console.log(`[${timestamp}]: Création d'une sauvegarde du site web du FTP dans le dossier ${backupFolderName}...`);
 
-    // Vérifie si le dossier backupWebsite existe
+    // Vérifie si le dossier backupWebsite/backupFolderName existe
     const exists = await fsExtra.pathExists(backupFolderPath);
-
     if (exists) {
-        // Si le dossier existe, le supprimer
+        // Si le dossier existe, le supprimer (optionnel)
         await fsExtra.remove(backupFolderPath);
     }
 
-    // Recréer le dossier backupWebsite
+    // Recréer le dossier backupWebsite/backupFolderName
     await fsExtra.ensureDir(backupFolderPath);
 
     // Configuration de la connexion SFTP et des options de téléchargement
@@ -32,18 +32,26 @@ async function backupWebsite() {
     const sftp = new SftpClient();
     await sftp.connect(sftpOptions);
 
-    // Télécharger le contenu du site web
+    // Assurez-vous que le dossier "backups" existe sur le serveur SFTP
+    await sftp.mkdir('/backups', true);
+
+    // Télécharger le contenu du site web dans le dossier de sauvegarde local
     await sftp.downloadDir('./', backupFolderPath);
 
-    await sftp.uploadDir('./backupWebsite', './backupWebsite');
+    // Téléverser le dossier de sauvegarde dans le dossier "backups" sur le serveur SFTP
+    if(process.env.GITHUB_BRANCH === 'main') {
+        await sftp.uploadDir(backupFolderPath, `/backups/${backupFolderName}`);
+    } else {
+        await sftp.uploadDir(backupFolderPath, `/dev/backups/${backupFolderName}`);
+    }
 
-    console.log('Backup du site web réalisé avec succès.');
+    console.log(`Backup du site web réalisé avec succès dans /backups/${backupFolderName}.`);
     sftp.end();
 }
 
 // Vous devrez adapter ou implémenter downloadDir pour ssh2-sftp-client
 // La fonction downloadDir ci-dessous est un placeholder et doit être implémentée correctement
-SftpClient.prototype.downloadDir = async function(sourceDir, destDir, excludeDirs = ['logs', 'cache', 'tmp', 'css', 'xivapi_poc', '.git', '.vscode', 'vendor', 'templates', 'guide', 'sitemap.xml', 'manifest.json', 'browserconfig.xml', '.gitignore', '404.html', 'backupBot', 'favicon.ico', 'backupWebsite', 'assets', 'dev']) {
+SftpClient.prototype.downloadDir = async function(sourceDir, destDir, excludeDirs = ['logs', 'cache', 'tmp', 'css', 'xivapi_poc', '.git', '.vscode', 'vendor', 'templates', 'guide', 'sitemap.xml', 'manifest.json', 'browserconfig.xml', '.gitignore', '404.html', 'backupBot', 'favicon.ico', 'backups', 'assets', 'dev']) {
     const handleDir = async (currentSourceDir, currentDestDir) => {
         await fsExtra.ensureDir(currentDestDir); // Assurez-vous que le répertoire de destination existe
 
@@ -69,7 +77,7 @@ SftpClient.prototype.downloadDir = async function(sourceDir, destDir, excludeDir
             }
         }
 
-        // Sauvegarde de tous les fichiers de speakers dans un fichier JSON, pour faire des vérifs plus tard.
+        /*// Sauvegarde de tous les fichiers de speakers dans un fichier JSON, pour faire des vérifs plus tard.
         let speakersFiles = await this.list('/assets/img/speakers')
         let data = JSON.stringify(speakersFiles, null, 2);
         let filePath = './backupWebsite/allFiles.json';
@@ -78,37 +86,41 @@ SftpClient.prototype.downloadDir = async function(sourceDir, destDir, excludeDir
                 console.error('Une erreur est survenue lors de la sauvegarde du fichier:', err);
             } else {
                 console.log('Fichier sauvegardé avec succès.');
-            }});
+            }});*/
 
 
     };
 
     await handleDir(sourceDir, destDir); // Commencer le traitement à partir du répertoire source
 
-SftpClient.prototype.uploadDir = async function(sourceDir, destDir, excludeDirs = []) {
-    const handleDir = async (currentSourceDir, currentDestDir) => {
-        console.log('Upload du répertoire:', currentSourceDir);
-        await this.mkdir(currentDestDir, true); // Crée le répertoire de destination si nécessaire
-
-        const items = await fsExtra.readdir(currentSourceDir); // Liste les fichiers/dossiers dans le répertoire courant
-
-        for (let itemName of items) {
+    SftpClient.prototype.uploadDir = async function(sourceDir, destDir, excludeDirs = []) {
+        const handleDir = async (currentSourceDir, currentDestDir) => {
+            console.log('Upload du répertoire:', currentSourceDir);
             
-            const itemSourcePath = path.join(currentSourceDir, itemName);
-            const itemDestPath = path.posix.join(currentDestDir, itemName); // Utilisez path.posix.join pour garantir l'utilisation de slash (/)
-
-            const stat = await fsExtra.stat(itemSourcePath);
-            if (stat.isDirectory()) { // Si l'élément est un dossier
-                await handleDir(itemSourcePath, itemDestPath); // Récursion pour gérer le contenu du dossier
-            } else if (stat.isFile()) { // Si l'élément est un fichier);
-                await this.fastPut(itemSourcePath, itemDestPath); // Téléverser le fichier
+            // Crée le répertoire de destination si nécessaire
+            // Assurez-vous de ne pas répéter le chemin du répertoire source dans le chemin du répertoire de destination
+            await this.mkdir(currentDestDir, true);
+    
+            const items = await fsExtra.readdir(currentSourceDir); // Liste les fichiers/dossiers dans le répertoire courant
+    
+            for (let itemName of items) {
+                const itemSourcePath = path.join(currentSourceDir, itemName);
+                const itemDestPath = path.posix.join(currentDestDir, itemName); // Utilisez path.posix.join pour garantir l'utilisation de slash (/)
+    
+                const stat = await fsExtra.stat(itemSourcePath);
+                if (stat.isDirectory()) { // Si l'élément est un dossier
+                    // Appel récursif avec le chemin correctement ajusté
+                    await handleDir(itemSourcePath, path.posix.join(currentDestDir, path.basename(itemSourcePath)));
+                } else if (stat.isFile()) { // Si l'élément est un fichier
+                    await this.fastPut(itemSourcePath, itemDestPath); // Téléverser le fichier
+                }
             }
-        }
+        };
+    
+        // Commencer le traitement à partir du répertoire source
+        // Assurez-vous que le chemin du répertoire de destination est correctement défini ici
+        await handleDir(sourceDir, destDir);
     };
-
-    await handleDir(sourceDir, destDir); // Commencer le traitement à partir du répertoire source
-};
-
 };
 
 module.exports = backupWebsite;
