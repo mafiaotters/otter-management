be0d849903c8a9d9d2f382462aee304e38e617ea
ARK-BLKCN <48032984+sernoz@users.noreply.github.com>
2024-09-13 18:44:48 +0200

    pourquoi clear

--- GillSystem/pourquoi.js
+++ GillSystem/pourquoi.js
@@ -1,6 +1,6 @@
 
 async function pourquoi(bot, interaction){
-    await interaction.editReply({content: "Pourquoi on mange des gills ? \n- Le gill est un poisson, \n- Il ressemble aux gils de FF \n- Dans Nemo, c'est le parrain d'un gang \n\nBref, on en raffole ! ", ephemeral: true});
+    await interaction.editReply({content: "Pourquoi les Loutres mangent des gills ? \n- Le gill est un poisson, \n- Il ressemble aux gils de FF \n- Dans Nemo, c'est le parrain d'un gang \n\nBref, on en raffole ! ", ephemeral: true});
 }
 
 module.exports = pourquoi;
