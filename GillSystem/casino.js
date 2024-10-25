c606db88d9eb913f0ee39dee29ade0b00b51832a
ARK-BLKCN <48032984+sernoz@users.noreply.github.com>
2024-10-25 21:48:27 +0200

    add casino system without good gain value

--- GillSystem/casino.js
+++ GillSystem/casino.js
@@ -1,5 +1,91 @@
-async function collecte(bot, interaction){
-    // Logique de collecte ici
-    await interaction.editReply({content: "Casino effectuée.", ephemeral: true});
+const updateUserGills = require('./updateUserGills');
+const { EmbedBuilder } = require('discord.js');
+
+const objects = [
+    {symbole: '🍓', coeff: 1}, 
+    {symbole:'🍪', coeff: 1}, 
+    {symbole:'🍑', coeff: 1},
+    {symbole: '🍉', coeff: 1}, 
+    {symbole:'🍒', coeff: 1}, 
+    {symbole:'🍌', coeff: 1}, 
+    {symbole:'🍐', coeff: 1},
+    {symbole: '🐟', coeff : 1}
+];
+
+function generateRandomLine() {
+    const line = [];
+    for (let i = 0; i < 3; i++) {
+        const randomIndex = Math.floor(Math.random() * objects.length);
+        line.push(objects[randomIndex].symbole);
+    }
+    return line.join(' • ');
 }
-module.exports = collecte;
+
+async function casino(bot, interaction) {
+    const gillsToSpend = Math.floor(Math.random() * (12 - 8 + 1)) + 8; // Dépense aléatoire entre 8 et 12 gills
+
+    // Mettre à jour le solde de gills de l'utilisateur
+    await updateUserGills(interaction.user, -gillsToSpend);
+
+    // Création de l'embed initial
+    let embed = new EmbedBuilder()
+        .setTitle('Machine à Sous')
+        .setDescription('La machine à sous tourne...')
+        .setColor('#0099ff');
+
+    // Envoyer l'embed initial
+    await interaction.editReply({ embeds: [embed], ephemeral: false });
+
+    // Simuler le temps de rotation de la machine à sous 3 fois
+    for (let i = 0; i < 3; i++) {
+        await new Promise(resolve => setTimeout(resolve, 1000)); // Attendre 1 seconde
+        const currentResult = `${generateRandomLine()}\n${generateRandomLine()}\n${generateRandomLine()}`;
+        embed.setDescription(`\n${currentResult}`);
+        await interaction.editReply({ embeds: [embed] });
+    }
+
+    // Générer les résultats de la machine à sous
+    const result = `${generateRandomLine()}\n${generateRandomLine()}\n${generateRandomLine()}\n-------------\n${generateRandomLine()}\n-------------`;
+
+    // Calculer les gains en utilisant les coefficients
+    const gains = calculateGains(result);
+
+    // Mise à jour de l'embed avec le résultat de la machine à sous et les gains
+    embed.setDescription(`${result}\nVous avez gagné ${gains} gills !`)
+        .setColor('#0099ff');
+
+    // Mise à jour du message avec le nouvel embed
+    await interaction.editReply({ embeds: [embed] });
+
+    // Mettre à jour le solde de gills de l'utilisateur avec les gains
+    await updateUserGills(interaction.user, gains);
+}
+
+function calculateGains(result) {
+    let gains = 0;
+    const lines = result.split('\n');
+    const lastLine = lines[lines.length - 2]; // Prendre uniquement l'avant dernière ligne, soit la dernière ligne avec symbole
+
+    console.log("Dernière ligne:", lastLine);
+
+    // Compter les occurrences de chaque symbole
+    const symbolCounts = {};
+    objects.forEach(obj => {
+        const count = (lastLine.match(new RegExp(obj.symbole, 'g')) || []).length;
+        symbolCounts[obj.symbole] = count;
+    });
+
+    // Calculer les gains en fonction des occurrences
+    objects.forEach(obj => {
+        const count = symbolCounts[obj.symbole];
+        if (count === 2) {
+            gains += obj.coeff + 2; // Gain pour 2 occurrences
+        } else if (count === 3) {
+            gains += obj.coeff + 3; // Gain pour 3 occurrences
+        }
+    });
+    console.log("Gains: " + gains);
+    return gains;
+}
+
+module.exports = casino;
--- bot.js
+++ bot.js
@@ -4,6 +4,8 @@ Pour rajouter des valeurs par défaut sur les membres, c'est dans Commands/add.j
 
 TODO:
 - Quand une personne qui a un grade concerné MAIS n'est pas dans la base de données firestore. Avertir par un message dans le chan bot.
+- dans updateMemberDAO, faire en sorte que ce soit aléatoire pour les membres.
+- Le filtre des mots, réalisé par Kronox -> Si un mot est détecté, y'a 10% de chance que Chantal dise un truc en mode "OMG TU DIS QUOI _feur_"
 */
 
 /*
@@ -11,7 +13,6 @@ A ACTIVER POUR L'ANNIVERSAIRE
 - Systeme de citations ( await saveQuote(message, bot) )
 - Déplacer gill.js dans "Commands"
 - Ré-activer l'update au démarrage
-
 */
 
 
@@ -82,6 +83,8 @@ console.log(`Nombre de listeners pour 'messageCreate' avant ajout: ${bot.listene
 bot.on('messageCreate', async (message) => {
   if (message.author.bot) return; // Ne pas répondre aux messages du bot lui-même
   if(!message.mentions.has(bot.user)) return; // Ne pas traiter les messages qui ne mentionnent pas le bot
+
+
 
   // Appeler saveQuote quand un message est reçu
   await saveQuote(message, bot);
