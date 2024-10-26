675e42d315bf6845597b94228d1329a3b6744b75
ARK-BLKCN <48032984+sernoz@users.noreply.github.com>
2024-10-26 14:04:25 +0200

    casino -> kaazino Rewards works.

--- GillSystem/kaazino.js
+++ GillSystem/kaazino.js
@@ -2,15 +2,28 @@ const updateUserGills = require('./updateUserGills');
 const { EmbedBuilder } = require('discord.js');
 
 const objects = [
-    {symbole: '🍓', coeff: 1}, 
-    {symbole:'🍪', coeff: 1}, 
-    {symbole:'🍑', coeff: 1},
-    {symbole: '🍉', coeff: 1}, 
-    {symbole:'🍒', coeff: 1}, 
-    {symbole:'🍌', coeff: 1}, 
-    {symbole:'🍐', coeff: 1},
-    {symbole: '🐟', coeff : 1}
+    {symbole: '🍓', coeff: 3}, 
+    {symbole:'🍪', coeff: 2.78}, 
+    {symbole:'🍑', coeff: 2.48},
+    {symbole: '🍉', coeff: 2.82}, 
+    {symbole:'🍒', coeff: 2.65}, 
+    {symbole:'🍌', coeff: 3.27}, 
+    {symbole:'🍐', coeff: 2.98},
+    {symbole: '🐟', coeff : 7.5}
 ];
+
+// FONCTION DE DEVELOPPEMENT SIMULATEGAINS - Permet de voir le gain par tentative, lissé.
+/*function simulateGains(numSimulations) {
+    console.warn("[DEV] Simulation des gains en cours...");
+    let totalGains = 0;
+    for (let i = 0; i < numSimulations; i++) {
+        const result = `${generateRandomLine()}\n${generateRandomLine()}\n${generateRandomLine()}\n-------------\n${generateRandomLine()}\n-------------`;
+        totalGains += calculateGains(result);
+    }
+    return totalGains / numSimulations;
+}
+console.log(`Gains moyens par tour: ${simulateGains(90000000)}`);*/
+
 
 function generateRandomLine() {
     const line = [];
@@ -21,7 +34,7 @@ function generateRandomLine() {
     return line.join(' • ');
 }
 
-async function casino(bot, interaction) {
+async function kaazino(bot, interaction) {
     const gillsToSpend = Math.floor(Math.random() * (12 - 8 + 1)) + 8; // Dépense aléatoire entre 8 et 12 gills
 
     // Mettre à jour le solde de gills de l'utilisateur
@@ -34,7 +47,7 @@ async function casino(bot, interaction) {
         .setColor('#0099ff');
 
     // Envoyer l'embed initial
-    await interaction.editReply({ embeds: [embed], ephemeral: false });
+    await interaction.editReply({ content: `:slot_machine::slot_machine::slot_machine: • <@${interaction.user.id}> envoie ${gillsToSpend} :fish: pour la machine à sous..`, embeds: [embed], ephemeral: false });
 
     // Simuler le temps de rotation de la machine à sous 3 fois
     for (let i = 0; i < 3; i++) {
@@ -44,18 +57,26 @@ async function casino(bot, interaction) {
         await interaction.editReply({ embeds: [embed] });
     }
 
-    // Générer les résultats de la machine à sous
+    // Générer le dernier embed de la machine à sous
     const result = `${generateRandomLine()}\n${generateRandomLine()}\n${generateRandomLine()}\n-------------\n${generateRandomLine()}\n-------------`;
 
     // Calculer les gains en utilisant les coefficients
     const gains = calculateGains(result);
 
-    // Mise à jour de l'embed avec le résultat de la machine à sous et les gains
+   /* // Mise à jour de l'embed avec le résultat de la machine à sous et les gains
     embed.setDescription(`${result}\nVous avez gagné ${gains} gills !`)
-        .setColor('#0099ff');
+        .setColor('#0099ff');*/
+
+    // Déterminer le texte à afficher en fonction des gains
+    let resultText = '';
+    if (gains === 0) {
+        resultText = 'et perd :otter_cry~1:';
+    } else {
+        resultText = 'et gagne ! :otter_pompom:';
+    }
 
     // Mise à jour du message avec le nouvel embed
-    await interaction.editReply({ embeds: [embed] });
+    await interaction.editReply({ content: `:slot_machine: • <@${interaction.user.id}> envoie ${gillsToSpend} :fish: pour la machine à sous... ${resultText}`, embeds: [embed], epheremal: false });
 
     // Mettre à jour le solde de gills de l'utilisateur avec les gains
     await updateUserGills(interaction.user, gains);
@@ -66,7 +87,7 @@ function calculateGains(result) {
     const lines = result.split('\n');
     const lastLine = lines[lines.length - 2]; // Prendre uniquement l'avant dernière ligne, soit la dernière ligne avec symbole
 
-    console.log("Dernière ligne:", lastLine);
+    //console.log("Dernière ligne:", lastLine);
 
     // Compter les occurrences de chaque symbole
     const symbolCounts = {};
@@ -79,13 +100,14 @@ function calculateGains(result) {
     objects.forEach(obj => {
         const count = symbolCounts[obj.symbole];
         if (count === 2) {
-            gains += obj.coeff + 2; // Gain pour 2 occurrences
+            gains += obj.coeff * 8; // Gain pour 2 occurrences
         } else if (count === 3) {
-            gains += obj.coeff + 3; // Gain pour 3 occurrences
+            gains += obj.coeff * 20; // Gain pour 3 occurrences
         }
+        gain = Math.floor(gains);
     });
-    console.log("Gains: " + gains);
+    //console.log("Gains: " + gains);
     return gains;
 }
 
-module.exports = casino;
+module.exports = kaazino;
