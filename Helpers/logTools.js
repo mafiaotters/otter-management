7d51ab97cea40346bbc11395bbc167f81e4f9954
ARK-BLKCN <48032984+sernoz@users.noreply.github.com>
2025-08-02 22:05:42 +0200

    UTC+2 dans les logs

--- Helpers/logTools.js
+++ Helpers/logTools.js
@@ -1,13 +1,16 @@
-async function dateFormatLog(){
+async function dateFormatLog() {
     const date = new Date();
-    
-    let formattedDate = `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getHours().toString().padStart(2, '0')}h${date.getMinutes().toString().padStart(2, '0')}`;
+
+    // Décalage UTC+2
+    const offset = 2; // heures
+    const utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+    const localDate = new Date(utc + (offset * 3600000));
+
+    let formattedDate = `${localDate.getDate().toString().padStart(2, '0')}/${(localDate.getMonth() + 1).toString().padStart(2, '0')}-${localDate.getHours().toString().padStart(2, '0')}h${localDate.getMinutes().toString().padStart(2, '0')}`;
     formattedDate = `[${formattedDate}] `;
 
     return formattedDate;
-
 }
-
 
 module.exports = {
     dateFormatLog
